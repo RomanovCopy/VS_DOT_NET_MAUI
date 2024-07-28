f4b8be6138b8172b36bacb7ff168a160856c2654
RomanovCopy <RomanovCopy@gmail.com>
2024-07-28 17:32:53 +0300

    fix

--- ScriptumSecuris/ScriptumSecuris/AppShell.xaml.cs
+++ ScriptumSecuris/ScriptumSecuris/AppShell.xaml.cs
@@ -6,5 +6,20 @@
         {
             InitializeComponent();
         }
+
+        private void OnOpenClicked(object sender, System.EventArgs e)
+        {
+            // Обработка нажатия на Open
+        }
+
+        private void OnSaveClicked(object sender, System.EventArgs e)
+        {
+            // Обработка нажатия на Save
+        }
+
+        private void OnSaveAsClicked(object sender, System.EventArgs e)
+        {
+            // Обработка нажатия на Save As
+        }
     }
 }
